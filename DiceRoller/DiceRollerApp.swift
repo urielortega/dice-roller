8d69b0ac37882038b16f017627e4102c9fdf20ba
Uriel Ortega <urielortega2509@gmail.com>
2023-07-18 13:22:21 -0600

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Uriel Ortega on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
